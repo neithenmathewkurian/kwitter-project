3a8c89f792d6fe3e3e611dd318e6daf45094462f
neithenmathewkurian <86046699+neithenmathewkurian@users.noreply.github.com>
2021-11-29 11:09:39 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,9 +1,15 @@
+const firebaseConfig = {
+	apiKey: "AIzaSyDw_GqH3QyLRIWpwhqreJ8k9CByAl4mbG0",
+	authDomain: "kwitter-71d98.firebaseapp.com",
+	databaseURL: "https://kwitter-71d98-default-rtdb.firebaseio.com",
+	projectId: "kwitter-71d98",
+	storageBucket: "kwitter-71d98.appspot.com",
+	messagingSenderId: "73801451639",
+	appId: "1:73801451639:web:d60c3e31c16c5c1a3a1e72"
+  };
 //ADD YOUR FIREBASE LINKS HERE
-
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-    Room_names = childKey;
-   //Start code
-
-   //End code
-   });});}
-getData()
+firebase.initializeApp(firebaseConfig);
+var name1=localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML="welcome "+name1+"!";
+function getData() {
+	firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
